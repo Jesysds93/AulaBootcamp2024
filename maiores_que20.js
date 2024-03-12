77a71a17402e7ae9b08da27d03906dd8beabb176
Jesysds93 <jesy.sds93@hotmail.com>
2024-03-12 19:52:34 -0300

    refactor

---
+++ maiores_que20.js
@@ -0,0 +1,10 @@
+// maiores_que_20.js
+
+const numeros = [9, 4, 5, 10, 2, 90, 8, 6, 33];
+
+// Filtrando os números maiores que 20
+const maioresQue20 = numeros.filter((numero) => numero > 20);
+
+// Exibindo os resultados
+console.log("Números maiores que 20:");
+console.log(maioresQue20);
