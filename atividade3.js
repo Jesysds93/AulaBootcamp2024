f1985cf67113fd710ab6c45b26dcf63cf98332f0
Jesiane Soares <160436696+Jesysds93@users.noreply.github.com>
2024-03-12 22:58:09 -0300

    Update atividade3.js

--- atividade3.js
+++ atividade3.js
@@ -1,8 +1,6 @@
+/*24. Use um loop for para percorrer o array numeros e exibir cada elemento no console.*/
 
-let idade = 19; 
-
-if (idade <= 18) {
-  console.log("Menor de idade");
-} else {
-  console.log("Maior de idade");
+const numeros = [1, 2, 3, 4, 5];
+for (let i = 0; i < numeros.length; i++) {
+    console.log(numeros[i]);
 }
